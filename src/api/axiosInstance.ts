7a792dca981c02446c0ea9a077b4fcb3e25e927b
YOUNGHO8762 <youngho8762@gmail.com>
2025-01-11 21:35:23 +0900

    get 공통화 함수 제작 및 적용

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export const axiosInstance = axios.create({
+const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
 });
+
+export const get = async <T>(url: string, params?: object): Promise<T> => {
+  const { data } = await instance.get<T>(url, { params });
+  return data;
+};
--- src/api/pokemonApi.ts
+++ src/api/pokemonApi.ts
@@ -1,17 +1,11 @@
-import { axiosInstance } from '@/api/axiosInstance';
+import { get } from '@/api/axiosInstance';
 import { Pokemon, PokemonsResponse } from '@/types/pokemon';
 import { POKEMON_URL } from '@/constants/apiUrl';
 
-export const fetchPokemons = async (
-  offset: number
-): Promise<PokemonsResponse> => {
-  const { data } = await axiosInstance.get<PokemonsResponse>(POKEMON_URL, {
-    params: { limit: 20, offset },
-  });
-  return data;
+export const fetchPokemons = async (offset: number) => {
+  return get<PokemonsResponse>(POKEMON_URL, { limit: 20, offset });
 };
 
 export const fetchPokemon = async (name: string): Promise<Pokemon> => {
-  const { data } = await axiosInstance.get<Pokemon>(`${POKEMON_URL}/${name}`);
-  return data;
+  return get<Pokemon>(`${POKEMON_URL}/${name}`);
 };
